7d388dfae74e6c5cc1763deba3d94e8c0d54bb7a
Ashwin Valento <Ashwinvalento@users.noreply.github.com>
2016-11-23 17:33:26 +0530

    Update contact_me.js
    Updated mail server info

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://valento.in/portfolio/mail/contact_me.php",
+                url: "http://valento.in/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
